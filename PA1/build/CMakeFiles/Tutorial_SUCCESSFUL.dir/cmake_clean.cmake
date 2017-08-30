9718b443e91347049cbf2a8fbd9504f05cacac9a
Sean Patrick McDonald <spmcdonald@nevada.unr.edu>
2017-08-29 19:32:33 -0700

    remove build folder

--- PA1/build/CMakeFiles/Tutorial_SUCCESSFUL.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/Tutorial_SUCCESSFUL"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/Tutorial_SUCCESSFUL.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
