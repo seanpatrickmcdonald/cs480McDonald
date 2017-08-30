cca8d38cd7616415edef894cbfd2960773ba86b7
Sean Patrick McDonald <spmcdonald@nevada.unr.edu>
2017-08-29 19:26:49 -0700

    Adding PA1

---
+++ PA1/build/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gylindril/Desktop/computer-graphics/PA1/src/camera.cpp" "/home/gylindril/Desktop/computer-graphics/PA1/build/CMakeFiles/Tutorial.dir/src/camera.cpp.o"
+  "/home/gylindril/Desktop/computer-graphics/PA1/src/engine.cpp" "/home/gylindril/Desktop/computer-graphics/PA1/build/CMakeFiles/Tutorial.dir/src/engine.cpp.o"
+  "/home/gylindril/Desktop/computer-graphics/PA1/src/graphics.cpp" "/home/gylindril/Desktop/computer-graphics/PA1/build/CMakeFiles/Tutorial.dir/src/graphics.cpp.o"
+  "/home/gylindril/Desktop/computer-graphics/PA1/src/main.cpp" "/home/gylindril/Desktop/computer-graphics/PA1/build/CMakeFiles/Tutorial.dir/src/main.cpp.o"
+  "/home/gylindril/Desktop/computer-graphics/PA1/src/object.cpp" "/home/gylindril/Desktop/computer-graphics/PA1/build/CMakeFiles/Tutorial.dir/src/object.cpp.o"
+  "/home/gylindril/Desktop/computer-graphics/PA1/src/shader.cpp" "/home/gylindril/Desktop/computer-graphics/PA1/build/CMakeFiles/Tutorial.dir/src/shader.cpp.o"
+  "/home/gylindril/Desktop/computer-graphics/PA1/src/window.cpp" "/home/gylindril/Desktop/computer-graphics/PA1/build/CMakeFiles/Tutorial.dir/src/window.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "UNIX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/SDL2"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
